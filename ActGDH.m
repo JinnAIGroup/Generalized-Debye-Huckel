a07c346c0bf578c0c502c582226234e3a2ea72f3
JL <55872611+jinnliu@users.noreply.github.com>
2022-09-05 13:26:37 +0800

    Add files via upload

--- ActGDH.m
+++ ActGDH.m
@@ -1,48 +1,48 @@
-% SYC, CLL, JLL, 2022.7.17 - 8.31
-% Chin-Lung Li, Shu-Yi Chou, Jinn-Liang Liu (2022) Prediction of activity coefficients in
-% water-methanol mixtures using a generalized Debye-Huckel model
+% Chin-Lung Li, Shu-Yi Chou, Jinn-Liang Liu 
+% Prediction of activity coefficients in water-methanol mixtures using
+% a generalized Debye-Huckel model, ArXiv
+% September 5, 2022
 clc
 clear
 
-ScSiz = get(0, 'ScreenSize');
-pos_ACFig    = [0.15*ScSiz(3) 0.05*ScSiz(4) 0.7*ScSiz(3) 0.4*ScSiz(4)];  % Activity coefficient window position and size
-pos_thetaFig = [0.15*ScSiz(3) 0.45*ScSiz(4) 0.7*ScSiz(3) 0.4*ScSiz(4)];  % Theta window position and size
-
 %--- Physical Constants [Table 1 in the paper]
 T = 298.15; epsln = 8.854187; e = 1.6022; mol = 6.022045; kB = 1.380649; kBTe = (kB*T/e)*0.0001; % T in K = 25 C
-V0 = 1; S2 = 0.1*mol*e/(kBTe*epsln);  % V0: unit volume, S2: scaling factor to make concentration in [Eq (4)] unitless
+V0 = 1; S2 = 0.1*mol*e/(kBTe*epsln);  % V0: unit volume, S2: scaling factor [(vi)]
+
+ScSz = get(0, 'ScreenSize');
+FigPos_gamma = [0.15*ScSz(3) 0.05*ScSz(4) 0.7*ScSz(3) 0.4*ScSz(4)];  % Figure Position for gamma
+FigPos_theta = [0.15*ScSz(3) 0.45*ScSz(4) 0.7*ScSz(3) 0.4*ScSz(4)];  % Figure Position for theta
 
 for salts = 1:3
-  %--- Solvent Parameters: diS: dielectric constant, Vx: volume of solvent molecule [(10)]
-  %    CxM (scalar): concentration in M then scaled by S2 to unitless
+  %--- Solvent Parameters: diS: dielectric constant [(11)], Vx: volume [(18)],
+  %    CxM (scalar): bulk concentration in M [(18)], 1: cation, 2: anion, 3: H2O, 4: MeOH [Step 1]
   [C3M, C4M, CxM, V3, V4, Vx, diS] = Solvent(0); % x = 0: solvent = pure H2O
-  C3M = C3M*S2; C4M = C4M*S2; CxM = CxM*S2; 
+  C3M = C3M*S2; C4M = C4M*S2; CxM = CxM*S2;  % scaled by S2 to unitless
 
   if salts == 1
-      salt = "NaF";  pos_subFig = [0.09 0.15 0.26 0.8];  % figure properties
+      salt = "NaF";  FigPos_SubP = [0.09 0.15 0.26 0.8];  % Figure Position of subplot
   elseif salts == 2
-      salt = "NaCl"; pos_subFig = [0.4 0.15 0.26 0.8];
+      salt = "NaCl"; FigPos_SubP = [0.4 0.15 0.26 0.8];
   elseif salts == 3
-      salt = "NaBr"; pos_subFig = [0.71 0.15 0.26 0.8];
+      salt = "NaBr"; FigPos_SubP = [0.71 0.15 0.26 0.8];
   end
   
-  %--- Born Radii: BornR0 in pure solvent (no salt) [(12), (13)]
+  %--- Born Radii: BornR0 in pure solvent (no salt) [(12), (13), Step 2]
   [BornR0, q1, q2, V1, V2, mM, D] = Born(salt, diS, 0);
 
-  %--- Activity Data to Fit: C1m (vector): concentration of cation (1 for cation,
-  %    2 for anion) in molality (m), gamma: mean activity [(16)]
+  %--- Activity Data to Fit: C1m (vector): concentration in molality (m), gamma: mean activity [(16)]
   [C1m, gamma] = DataFit(salt); g_data = log(gamma);
 
-  %--- Salt molality (m) to Molarity (M): C1m to C1M in unitless M
+  %--- Salt molality (m) to Molarity (M): C1m to C1M [Step 3]
   C1M = m2M(C1m, mM, D, 0).*S2;
   
   C2M = q1*C1M;
-  BPfrac = (V1*C1M + V2*C2M + V3*C3M + V4*C4M)/S2/1660.6;  % Bulk Particle fraction
+  BPfrac = (V1*C1M + V2*C2M + V3*C3M + V4*C4M)/S2/1660.6;  % Bulk Particle fraction [(2)]
 
-  %--- Newton() iteratively solves nonlinear [(18)] for V_sh that yields Rsh_c and Rsh_a. 
+  %--- Newton() iteratively solves nonlinear [(18)] for V_sh that yields Rsh_c and Rsh_a [Step 4]. 
   [Rsh_c, Rsh_a] = Newton(C1M, CxM, V0, V1, V2, Vx, S2, BPfrac);
   
-  %--- LSfit() returns g_fit as the best fit to g_data with alpha(1), (2), (3) [(14)] by Least Squares.
+  %--- LSfit() returns g_fit as the best fit to g_data with alpha(1), (2), (3) [(14)] by Least Squares [Step 5].
   [g_fit, alpha, theta] = LSfit(g_data, BornR0, Rsh_c, Rsh_a, salt, C1M, C3M, C4M, q1, q2, V0, V1, V2, V3, V4, diS, T);
 
   %format long  % for all decimals in alpha
@@ -50,20 +50,20 @@ for salts = 1:3
   
   plot_theta = (1 + (C1M.^0.5)*alpha(1) + C1M*alpha(2) + (C1M.^1.5)*alpha(3));  % for plotting theta [(14)]
   
-  figure(1)  % Plot data points and fitted curves
-  set(gcf,'Position', pos_ACFig)  %set AC figure position and size
-  subplot('Position', pos_subFig)  %set subfigure position and size
+  figure(1)  % plot data points and fitted curves
+  set(gcf, 'Position', FigPos_gamma)
+  subplot('Position', FigPos_SubP)
   plot(C1m, g_data, 'k.');  % data points
   hold on
-  plot(C1m, g_fit, '-r');  % fitted curve from LSfit
-  text((C1m(end)), g_data(end)-0.02, 'x=0'); % notation of solvent
+  plot(C1m, g_fit, '-r');  % fitted curve
+  text((C1m(end)), g_data(end)-0.02, 'x=0');
   ylabel('ln\gamma_\pm', 'FontSize', 12);
   xlabel('m (mol/kg)', 'FontSize', 12); title(salt);
    
-  figure(2)  % Plot theta
-  set(gcf, 'Position', pos_thetaFig)  % set theta figure position and size
-  subplot('Position', pos_subFig) % set subfigure position and size
-  plot(C1m, plot_theta, '-r', 'LineWidth', 2, 'MarkerSize', 8, 'MarkerFaceColor', 'w') % theta plot
+  figure(2)  % plot theta
+  set(gcf, 'Position', FigPos_theta)
+  subplot('Position', FigPos_SubP)
+  plot(C1m, plot_theta, '-r', 'LineWidth', 2, 'MarkerSize', 8, 'MarkerFaceColor', 'w') % theta
   ylabel('\theta', 'FontSize', 12);
   xlabel('m (mol/kg)', 'FontSize', 12); title(salt);
   
@@ -85,24 +85,24 @@ for salts = 1:3
         x = 1.0; C1m_x = C1mX5; g_data_x = log(g_dataX5);
     end
     
-    [C3M, C4M, CxM, V3, V4, Vx, diS] = Solvent(x);
+    [C3M, C4M, CxM, V3, V4, Vx, diS] = Solvent(x);  % [Step 6]
     C3M = C3M*S2; C4M = C4M*S2; CxM = CxM*S2; 
 
-    [BornR0, q1, q2, V1, V2, mM, D] = Born(salt, diS, x);
-    C1M = m2M(C1m_x, mM, D, x).*S2;
+    [BornR0, q1, q2, V1, V2, mM, D] = Born(salt, diS, x);  % [Step 7]
+
+    C1M = m2M(C1m_x, mM, D, x).*S2;  % [Step 7]
     
     C2M = q1*C1M;
     BPfrac = (V1*C1M + V2*C2M + V3*C3M + V4*C4M)/S2/1660.6;
  
-    [Rsh_c, Rsh_a] = Newton(C1M, CxM, V0, V1, V2, Vx, S2, BPfrac);
+    [Rsh_c, Rsh_a] = Newton(C1M, CxM, V0, V1, V2, Vx, S2, BPfrac);  % [Step 7]
 
     alpha_x = alpha + x*delta_alpha;
     theta = 1 + alpha_x(1)*power(C1M, 1/2) + alpha_x(2)*C1M + alpha_x(3)*power(C1M, 3/2);
 
-    % Given theta, Activity() returns a mean activity. 
+    %--- Given theta, Activity() returns a mean activity [Step 8]. 
     g_pred_x = Activity(theta, BornR0, Rsh_c, Rsh_a, C1M, C3M, C4M, q1, q2, V0, V1, V2, V3, V4, diS, T);
 
-    %plot_theta_x = roundn(1 + power(C1M,1/2)*alpha_x(1) + C1M*alpha_x(1) + power(C1M,3/2)*alpha_x(3), -5);
     plot_theta_x = round( 1 + power(C1M,1/2)*alpha_x(1) + C1M*alpha_x(2) + power(C1M,3/2)*alpha_x(3), 5 );
     
     figure(1)  % Plot data points and predicted curves
@@ -134,31 +134,31 @@ function [C3M, C4M, CxM, V3, V4, Vx, diS] = Solvent(x)
   Vx  = (1-x)*V3 + x*V4;  % [(18)]
   C3M = (1-x)*55.5;
   C4M = x*24.55;
-  CxM = (1-x)*55.5  + x*24.55;
-  diS = (1-x)*78.45 + x*32.66;
+  CxM = (1-x)*55.5  + x*24.55;  % [(18)]
+  diS = (1-x)*78.45 + x*32.66;  % [(11)]
 end
 
 
 function [BornR0, q1, q2, V1, V2, mM, D] = Born(salt, diS, x)
-  epsln = 8.854187; e = 1.6022; mol = 6.022045;
-  if salt == "NaCl"  % Free energy: Na+ in H2O = -103.2 MeOH = -91.9; Cl- in H2O = -74.5 MeOH = -81.1
-    FcatH2O = -103.2; FcatMeOH = -91.9; FanH2O = -74.5; FanMeOH = -81.1;
-    q1 = 1; q2 = -1; V1 = 4*pi*0.95^3/3; V2 = 4*pi*1.81^3/3; mM = 58.44; D = 46.62; % mM: molar mass   D: density gradient of solution
+  epsln = 8.854187; e = 1.6022; mol = 6.022045;  % [Table 1]
+  if salt == "NaCl"   % Free energy: Na+ in H2O = -103.2 MeOH = -91.9; Cl- in H2O = -74.5 MeOH = -81.1
+    FcatH2O = -103.2; FcatMeOH = -91.9; FanH2O = -74.5; FanMeOH = -81.1;  % mM: molar mass
+    q1 = 1; q2 = -1; V1 = 4*pi*0.95^3/3; V2 = 4*pi*1.81^3/3; mM = 58.44; D = 46.62;  % D: density gradient of solution 
   elseif  salt == "NaBr"
     FcatH2O = -103.2; FcatMeOH = -91.9; FanH2O = -68.3; FanMeOH = -75.1;
-    q1 = 1; q2 = -1; V1 = 4*pi*0.95^3/3; V2 = 4*pi*1.95^3/3; mM = 102.89; D = 77; % mM: molar mass
+    q1 = 1; q2 = -1; V1 = 4*pi*0.95^3/3; V2 = 4*pi*1.95^3/3; mM = 102.89; D = 77;
   elseif  salt == "NaF"
     FcatH2O = -103.2; FcatMeOH = -91.9; FanH2O = -104.4; FanMeOH = -109.2;
-    q1 = 1; q2 = -1; V1 = 4*pi*0.95^3/3; V2 = 4*pi*1.36^3/3; mM = 41.99; D = 41.38; % mM: molar mass
+    q1 = 1; q2 = -1; V1 = 4*pi*0.95^3/3; V2 = 4*pi*1.36^3/3; mM = 41.99; D = 41.38;
   else
     FcatH2O = -103.2; FcatMeOH = -91.9; FanH2O = -74.5; FanMeOH = -81.1;
-    q1 = 1; q2 = -1; V1 = 4*pi*r1^3/3; V2 = 4*pi*r2^3/3; mM = 58.44; D = 46.62; % mM: molar mass, D: density gradient of solution
+    q1 = 1; q2 = -1; V1 = 4*pi*r1^3/3; V2 = 4*pi*r2^3/3; mM = 58.44; D = 46.62;
     warning('Warning: Salt not found.')
   end
-  F_cation = (1-x)*FcatH2O + x*FcatMeOH;
-  F_anion  = (1-x)*FanH2O  + x*FanMeOH;
-  BornR0c = q1*q1*e*e*mol*10000*(1-1/diS)/(-F_cation*4.1868*8*pi*epsln);  % Born radius of cation
-  BornR0a = q2*q2*e*e*mol*10000*(1-1/diS)/(-F_anion*4.1868*8*pi*epsln);   % Born radius of anion
+  Fcat = (1-x)*FcatH2O + x*FcatMeOH;
+  Fan  = (1-x)*FanH2O  + x*FanMeOH;
+  BornR0c = -q1*q1*e*e*mol*10000*(1-1/diS)/(Fcat*4.1868*8*pi*epsln);  % Born radius of cation [(12)]
+  BornR0a = -q2*q2*e*e*mol*10000*(1-1/diS)/(Fan*4.1868*8*pi*epsln);   % Born radius of anion
   BornR0 = [BornR0c BornR0a];
 end
 
@@ -167,7 +167,7 @@ function M = m2M(m, mM, D, x)
   % 0.9971 = density of water
   % 0.9128 = density of 50-50 water-methanol mixture
   % 0.7866 = density of methanol
-  rho_0 = (x-0.5)*(x-1)/0.5*0.9971 - x*(x-1)/0.25*0.9128 + x*(x-0.5)/0.5*0.7866;  % solvent density by Lagrange polynomial 
+  rho_0 = (x-0.5)*(x-1)/0.5*0.9971 - x*(x-1)/0.25*0.9128 + x*(x-0.5)/0.5*0.7866;  % Lagrange polynomial [Step 3] 
   rho_s = rho_0 + D*m/1000;  % solution density = solvent density + salt density
   M = 1000*m.*rho_s./(1000 + m.*mM);  % molality to Molarity
 end
